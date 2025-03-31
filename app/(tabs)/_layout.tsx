d46c9651ef07f98c912975362c17b0f9d05760f5
Zahraa222 <zahraaaa@umich.edu>
2025-03-31 19:07:17 -0400

    Update dependencies + remove template

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -2,14 +2,15 @@ import { Tabs } from 'expo-router';
 import React from 'react';
 import { Platform } from 'react-native';
 
-import { HapticTab } from '@/components/HapticTab';
-import { IconSymbol } from '@/components/ui/IconSymbol';
-import TabBarBackground from '@/components/ui/TabBarBackground';
-import { Colors } from '@/constants/Colors';
-import { useColorScheme } from '@/hooks/useColorScheme';
+import { HapticTab } from '../../components/HapticTab';
+import { IconSymbol } from '../../components/ui/IconSymbol';
+import TabBarBackground from '../../components/ui/TabBarBackground';
+import { Colors } from '../../constants/Colors';
+import { useColorScheme } from '../../hooks/useColorScheme';
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
+  console.log("🔁 Root Layout Loaded");
 
   return (
     <Tabs
---
+++ metro.config.js
@@ -0,0 +1,7 @@
+// Learn more: https://docs.expo.dev/guides/using-expo-client/#bare-workflow
+
+const { getDefaultConfig } = require('@expo/metro-config');
+
+const config = getDefaultConfig(__dirname);
+
+module.exports = config;
